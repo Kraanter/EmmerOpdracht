a4e9842208bd90bb3813443d7ee56e14bea625eb
Kraanter <krijngrimme@hotmail.com>
2023-09-22 14:26:18 +0200

    Updated naming

--- Interfaces/IContainer.cs
+++ Interfaces/IContainer.cs
@@ -2,9 +2,11 @@
 
 public interface IContainer
 {
+    // The maximum size of a container
     public int Capacity { get; }
+    // The amount the container is filled
     public int Size { get; set; }
 
-    public void Fill(int hoeveelheid);
+    public void Fill(int units);
     public void Empty(int units);
 }
